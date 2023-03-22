ea3ec64ceb6aa4e3e5488c3806e8db14517ea9c1
si-yu Lai <coffee30456@gmail.com>
2023-03-22 18:29:47 +0800

    手動輸入藥袋資訊

---
+++ app/src/main/java/com/junting/drug_android_frontend/ui/drugRecords/InputManuallyActivity.kt
@@ -0,0 +1,13 @@
+package com.junting.drug_android_frontend.ui.drugRecords
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.junting.drug_android_frontend.R
+
+class InputManuallyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_manually)
+
+    }
+}
